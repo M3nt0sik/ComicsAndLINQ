95d9c0aaf576f101dd2002e985a5c983fabbcafc
Kamil <mentos.96@o2.pl>
2023-06-30 21:26:05 +0200

    Write main function

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
-﻿internal class Program
+﻿using ComicsAndLINQ;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        IEnumerable<Comic> mostExpensive =
+            from comic in Comic.Catalog
+            where Comic.Prices[comic.Issue] > 500
+            orderby Comic.Prices[comic.Issue] descending
+            select comic;
+
+        foreach (Comic comic in mostExpensive)
+        {
+            Console.WriteLine($"{comic} jest wart {Comic.Prices[comic.Issue]:c}.");
+        }
     }
 }
